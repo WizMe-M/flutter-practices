b6540fb768f5eddfed568d33fb261080c2c29037
Timkin Maxim <enderman.2016@mail.ru>
2022-09-25 17:51:51 +0300

    spacing image

--- practice_1/lib/screen_1.dart
+++ practice_1/lib/screen_1.dart
@@ -8,119 +8,108 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.cyan,
-        width: double.infinity,
-        child: SafeArea(
-          child: Column(
-            children:  [
-              Expanded(
-                flex: 2,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Image.asset('assets/medinow.png'),
-                      ),
-                      const Text('Meditate With Us!',
-                        style: TextStyle(
+        body: Container(
+      color: Colors.cyan,
+      width: double.infinity,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Image.asset('assets/medinow.png'),
+                    ),
+                    const Text(
+                      'Meditate With Us!',
+                      style: TextStyle(
                           color: Colors.white,
                           fontSize: 20,
-                          fontWeight: FontWeight.w400
-                        ),
-                      )
-                    ],
-                  ),
+                          fontWeight: FontWeight.w400),
+                    )
+                  ],
                 ),
               ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(45)
-                        )
-                      ),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Colors.white
-                      ),
-                      fixedSize: MaterialStateProperty.all<Size>(
-                        const Size.fromHeight(50)
-                      ),
-                    ),
-                    child: const Text('Sign in with Apple',
+                            borderRadius: BorderRadius.circular(45))),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    fixedSize: MaterialStateProperty.all<Size>(
+                        const Size.fromHeight(50)),
+                  ),
+                  child: const Text(
+                    'Sign in with Apple',
                     textAlign: TextAlign.center,
-                      style: TextStyle(
+                    style: TextStyle(
                         color: Colors.black,
                         fontSize: 20,
-                        fontWeight: FontWeight.w500
-                      ),
-                    ),
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+              child: SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                         RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(45)
-                        )
-                      ),
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Colors.cyan.shade100
-                      ),
-                      fixedSize: MaterialStateProperty.all<Size>(
-                        const Size.fromHeight(50)
-                      ),
-                    ),
-                    child: const Text('Continue with Email or Phone',
+                            borderRadius: BorderRadius.circular(45))),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.cyan.shade100),
+                    fixedSize: MaterialStateProperty.all<Size>(
+                        const Size.fromHeight(50)),
+                  ),
+                  child: const Text(
+                    'Continue with Email or Phone',
                     textAlign: TextAlign.center,
-                      style: TextStyle(
+                    style: TextStyle(
                         color: Colors.black,
                         fontSize: 20,
-                        fontWeight: FontWeight.w500
-                      ),
-                    ),
+                        fontWeight: FontWeight.w500),
                   ),
                 ),
               ),
-
-              TextButton(
-                onPressed: () {},
-                child: const Text('Continue With Google',
-                  style: TextStyle(
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Continue With Google',
+                style: TextStyle(
                     color: Colors.white,
                     fontSize: 18,
-                    fontWeight: FontWeight.normal
-                  ),
-                ),
+                    fontWeight: FontWeight.normal),
               ),
-        
-              Expanded(
-                flex: 3,
-                child: Image.asset('assets/meditation.png')
-              )
-            ],
-          ),
+            ),
+            const Spacer(),
+            Image.asset(
+              'assets/meditation.png',
+              fit: BoxFit.fill,
+              width: 300,
+            ),
+            const Spacer()
+          ],
         ),
-      )
-    );
+      ),
+    ));
   }
 }
