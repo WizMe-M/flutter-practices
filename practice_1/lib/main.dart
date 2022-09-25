b459f390937adccbdf2261a77cc4b3810c1a5167
Timkin Maxim <enderman.2016@mail.ru>
2022-09-25 17:55:16 +0300

    scrollable meditate screen

--- practice_1/lib/main.dart
+++ practice_1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice_1/screen_1.dart';
+import 'package:practice_1/screen_2.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Practice 1',
-      home: WelcomeScreen(),
+      home: MeditateScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
