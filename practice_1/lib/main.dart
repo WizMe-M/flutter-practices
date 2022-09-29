73792581178352948afb67c6347bcdd7bdd723e2
Timkin Maxim <enderman.2016@mail.ru>
2022-09-29 21:41:47 +0300

    flutter plugin update

--- practice_1/lib/main.dart
+++ practice_1/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:practice_1/screen_1.dart';
-import 'package:practice_1/screen_2.dart';
-import 'package:practice_1/screen_3.dart';
+import 'package:practice_1/welcome_screen.dart';
+import 'package:practice_1/meditate_screen.dart';
+import 'package:practice_1/main_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
